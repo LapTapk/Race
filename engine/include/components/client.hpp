8b198ae92039ea53df76b202d48b2e23710efdc9
korobokk <amsavitskii@edu.hse.ru>
2024-06-14 21:14:33 +0300

    added documentation to server's components

--- engine/include/components/client.hpp
+++ engine/include/components/client.hpp
@@ -12,20 +12,36 @@
 #include "server_io.hpp"
 
 using boost::asio::ip::tcp;
-
+/**
+ * Класс, отвечающий за создание и отправку сообщений серверу
+ * Обладает компонентами, определяющими его поведение
+*/
 class Client : public Component {
 public:
-    ServerIO& sio;
-    std::vector<sf::Vector2f> coords;
-    std::vector<GameObject*> players;
-    int this_ind;
-    std::string create_mes();
+    ServerIO& sio; ///> Обьект, позволяющий осуществлять подключение к серверу
+    std::vector<sf::Vector2f> coords; ///> координаты игрока, которые будут переданы серверу
+    std::vector<GameObject*> players; ///> Обьекты игроков, позиции которых должны быть изменены
+    int this_ind; ///> Индекс главного игрока
+    std::string create_mes(); ///> Функция, вызываемая для создания сообщения
+    /**
+     * Функция парсинга, полученных от сервера, координат
+     * 
+     * @param mes строковое сообщение, полученное от сервера, из которого необходимо получить координаты
+    */
     void parse_coords(
         std::string mes);
+    /**
+     * Конструктор класса клиента
+     * 
+     * @param go Указатель на GameObject, который является родительским объектом
+     * @param sio Параметр подключения к серверу, позволяющий осуществлять отправку и получение сообщений
+     * @param this_ind Индекс главного игрока
+     * @param players Вектор из обьектов других игроков, учавствующих в игре
+    */
     Client(GameObject* go, ServerIO& sio,
         int this_ind, std::vector<GameObject*> players);
-    void set_positions();
-    void update() override;
+    void set_positions(); ///> функция, вызываемая для обновлений координат игроков
+    void update() override; ///> функция обработки кадра
 };
 
 #endif
